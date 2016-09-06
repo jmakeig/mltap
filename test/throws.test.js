2fb24a2de90f773f686c1ccdb614e170006f04c3
Justin Makeig <jmpublic+github@makeig.com>
2016-09-05 22:14:36 -0700

    Fixes #3 with throws assertion

--- test/throws.test.js
+++ test/throws.test.js
@@ -5,14 +5,19 @@ const remote = require('../marklogic-remote')(/* connection */);
 const parseTAP = require('../lib/tap-helpers').parseTAP;
 
 test('assert.throws()', (assert) => {
-  assert.plan(1);
+  assert.plan(7);
   remote('test/throws.test.sjs') // CHANGE ME
-    .then((tap) => {
-      //assert.comment(tap.length);
-      parseTAP(tap)
-        .then(tree => assert.true('object' === typeof tree, 'Non-null parse tree (baby steps)'))
-        .catch(error => assert.fail(error));
+    .then((tap) => parseTAP(tap))
+    .then(tap => {
+      assert.equal(tap.count, 3, 'Three total tests run');
+      assert.equal(tap.pass, 2, 'Two total tests pass');
+      assert.equal(tap.fail, 1, 'Two total tests fail');
+      const failure = tap.failures[0];
+      assert.skip(failure.diag.operator, 'throws', 'Operator is throws');
+      assert.equal(failure.diag.expected, 'ReferenceError');
+      assert.equal(failure.diag.actual, 'TypeError');
+      assert.equal(failure.diag.at, '/test/throws.test.sjs:17:19', 'Failure location')
     })
-    .catch((error, stderr) => assert.fail(error.body.errorResponse.message));
+    .catch(error => assert.fail(error));
 });
 
