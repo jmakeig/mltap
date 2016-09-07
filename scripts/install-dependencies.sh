7961612107a872020a2e6ce3b7a2673ed93c983a
Justin Makeig <jmpublic+github@makeig.com>
2016-09-07 08:05:03 -0700

    Fixes paths to Travis scripts

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
-  ./shared/dev-tasks/travis-install-ml.sh nightly
-  ./shared/dev-tasks/setup-marklogic.sh
+  ./scripts/travis-install-ml.sh nightly
+  ./scripts/setup-marklogic.sh
 fi
