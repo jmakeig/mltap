b80e96b189b58a58fdcd7e2000df5c24ba92cc33
Justin Makeig <jmpublic+github@makeig.com>
2016-09-07 08:19:43 -0700

    Another try at the nightly URL

--- scripts/travis-install-ml.sh
+++ scripts/travis-install-ml.sh
@@ -51,14 +51,15 @@ else
   echo "********* Downloading MarkLogic nightly $ver"
 
   # fetch/install ML nightly
-  fname="MarkLogic-$ver.x86_64.rpm"
+  #fname="MarkLogic-$ver.x86_64.rpm"
+  fname="MarkLogic-RHEL6-$ver.x86_64.rpm"
   fnamedeb="marklogic_"
   fnamedeb=$fnamedeb$ver
   suff="_amd64.deb"
   fnamedeb=$fnamedeb$suff
 
   #url="https://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/b8_0/pkgs.$day/$fname"
-  url="https://root.marklogic.com/nightly/builds/linux64/rh6v-intel64-90-test-1.marklogic.com/HEAD/pkgs.$day/MarkLogic-RHEL6-$fname"
+  url="https://root.marklogic.com/nightly/builds/linux64/rh6v-intel64-90-test-1.marklogic.com/b9_0/pkgs.$day/$fname"
   echo "$url"
 
   status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
