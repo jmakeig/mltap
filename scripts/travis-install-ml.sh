6c483c56aaad7b4fffb98c7110d3ef0f3abc527c
Justin Makeig <jmpublic+github@makeig.com>
2016-09-15 09:22:27 -0700

    Changes MarkLogic install to use HEAD, not b9_0

--- scripts/travis-install-ml.sh
+++ scripts/travis-install-ml.sh
@@ -59,7 +59,7 @@ else
   fnamedeb=$fnamedeb$suff
 
   #url="https://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/b8_0/pkgs.$day/$fname"
-  url="https://root.marklogic.com/nightly/builds/linux64/rh6v-intel64-90-test-1.marklogic.com/b9_0/pkgs.$day/$fname"
+  url="https://root.marklogic.com/nightly/builds/linux64/rh6v-intel64-90-test-1.marklogic.com/HEAD/pkgs.$day/$fname"
   echo "$url"
 
   status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
