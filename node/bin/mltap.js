3edd120eebea7e831348828704ec9ee357ea73ac
Justin Makeig <jmpublic+github@makeig.com>
2016-09-18 01:57:28 -0700

    Parameterizes MarkLogic connection on the command line (#26)
    * Looks for command-line options, then environment variables, then falls back to defaults.
    * Prompts for password if none is specified for digest and basic auth.
    * Refactors how the connection object is built from arguments.
    
    Fixes #26

--- node/bin/mltap.js
+++ node/bin/mltap.js
@@ -2,36 +2,74 @@
 
 const doc =
 `
-Tape-like JavaScript test runner for MarkLogic.
+A MarkLogic unit test framework, compatible with the Node.js tape library.
 
 Usage:
-  mltap <test>...
-  mltap <test>... --output <file>
-  mltap <test>... --stdout
-  mltap --help
+  mltap [-u <user>] [-p <password>] [-d <database>] [-h <host>] [-P <port>] <tests>...
 
 Options:
-  <test>                         A list of tests, usually a glob
-  -o <file>, --output <file>     Send output to a file
-  --stdout                       Send to standard out
-  -h, --help                     Help
+  -u --user=<user>           MarkLogic user running the tests
+  -p --password=<password>   Password for MarkLogic user
+  -d --database=<database>   The database name
+  -h --host=<host>           MarkLogic host [default: localhost]
+  -P --port=<port>           MarkLogic host port [default: 8000]
+  --help                     
+  --version                  
 `;
 
 const opts = require('docopt').docopt(doc);
 
-// TODO: Parameterize me
-const conn = {
-  host:     'localhost',
-  port:     '8000',
-  user:     'tester',
-  password: 'tester', 
-  authType: 'digest',
+/**
+ * If the connection object lacks a password, prompt for it and 
+ * resolve with a new connection object.
+ * 
+ * @param {object} conn  MarkLogic connection object
+ *     { '--database': 'Documents',
+ *       '--host': 'localhost',
+ *       '--password': null,
+ *       '--port': '8000',
+ *       '--user': 'asdf',
+ *       '<tests>': [ 'test.test.sjs' ] }
+ * @returns Promise      A promise to return a MarkLogic connection object
+ */
+function prepareConnection(opts) {
+
+  /* 
+  * MarkLogic connection settings. First commandline options, then
+  * environment variables, then defaults. 
+  */
+  const conn = {
+    host:     opts['--host']               || process.env['MLTAP_HOST']     || 'localhost',
+    port:     parseInt(opts['--port'], 10) || process.env['MLTAP_PORT']     || 8000,
+    user:     opts['--user']               || process.env['MLTAP_USER']     || 'tester',
+    password: opts['--password']           || process.env['MLTAP_PASSWORD'] || 'tester', 
+    database: opts['--database']           || process.env['MLTAP_DATABASE'] || 'Documents',
+    authType: 'digest', // TODO: Parameterize me
+  };
+  return new Promise((resolve, reject) => {
+    if(['digest', 'basic'].indexOf(conn.authType) > -1 && !conn.password) {
+      const getPass = require('getpass').getPass;
+      const prompt = {prompt: `Password for ${conn.user} (${conn.host})`};
+      getPass(prompt, (error, password) => {
+        if(error) {
+          reject(error);
+        } else {
+          resolve(
+            Object.assign({}, conn, {password: password})
+          );
+        }
+      });
+    } else {
+      resolve(conn);
+    }
+  });
 }
 
-const remote = require('../marklogic-remote')(conn);
-
-remote(opts['<test>'], process.cwd())
-  //.then(tap => process.stdout.write(tap + '\n'))
+prepareConnection(opts)
+  .then(conn => {
+    const remote = require('../marklogic-remote')(conn);
+    return remote(opts['<tests>'], process.cwd());
+  })
   .then(tap => process.stdout.write(tap + '\n'))
   .catch(error => { 
     // console.dir(error);
