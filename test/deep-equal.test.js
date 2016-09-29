912e9e75e77b1772b043f8b81a8a71fb1fceb7bd
Justin Makeig <jmpublic+github@makeig.com>
2016-09-29 00:13:39 -0700

    Implements skipped tests
    Depended on serialization from #19

--- test/deep-equal.test.js
+++ test/deep-equal.test.js
@@ -9,14 +9,14 @@ test('assert.deepEqual()', (assert) => {
   remote('test/deep-equal.test.sjs') // CHANGE ME
     .then((tap) => parseTAP(tap))
     .then(tap => {
-      assert.equal(tap.count, 2);
-      assert.equal(tap.pass, 1);
-      assert.equal(tap.fail, 1);
+      assert.equal(tap.count, 2, 'count');
+      assert.equal(tap.pass, 1, 'pass');
+      assert.equal(tap.fail, 1, 'fail');
       const failure = tap.failures[0];
-      assert.skip(failure.diag.expected, '?'); // Depends on #19
-      assert.skip(failure.diag.actual, '?'); // Depends on #19
-      assert.equal(failure.diag.at, 'Test.impl (/mltap/mltap.js:7:10)'); // Depends on #20
-      assert.equal(failure.diag.operator, 'deepEqual', 'Operator is deepEqual');
+      assert.equal(failure.diag.expected, 'Array []', 'expected Array');
+      assert.equal(failure.diag.actual, 'Object {\n  "a": "A",\n}', 'actual Object');
+      assert.equal(failure.diag.at, 'Test.impl (/mltap/mltap.js:7:10)', 'at stack frame');
+      assert.equal(failure.diag.operator, 'deepEqual', 'operator is deepEqual');
     })
     .catch(error => assert.fail(error));
 });
